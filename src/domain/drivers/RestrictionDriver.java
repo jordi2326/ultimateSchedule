47486e1ef70da4ae92223128a4c8bee0627efe77
xlacasa <xlcbdn@gmail.com>
2018-11-16 03:07:01 +0100

    Restriction drivers missing only testValidate

--- src/domain/drivers/RestrictionDriver.java
+++ src/domain/drivers/RestrictionDriver.java
@@ -1,8 +1,16 @@
 package domain.drivers;
 import java.util.Scanner;
 
+import domain.classes.Group;
 import domain.classes.Restriction;
-/*
+import domain.classes.restrictions.CorequisitRestriction;
+import domain.classes.restrictions.DayPeriodRestriction;
+import domain.classes.restrictions.LectureFromSameGroupOverlapRestriction;
+import domain.classes.restrictions.OccupiedRoomRestriction;
+import domain.classes.restrictions.ParentGroupOverlapRestriction;
+import domain.classes.restrictions.SpecificDayOrHourRestriction;
+import domain.classes.restrictions.SubjectLevelRestriction;
+
 
 public class RestrictionDriver {
 	private static Restriction r;
@@ -17,6 +25,7 @@ public class RestrictionDriver {
 	            + " 3| enable\n"
 	            + " 4| isEnabled\n"
 	            + " 5| isNegotiable\n"
+	            + " 6| validate\n"
 	            + " 0| Salir\n"
 	            + "---------------------\n"
 	            );
@@ -46,26 +55,101 @@ public class RestrictionDriver {
 	            case 5:
 	            	testIsNegotiable(sc);
 	                break;
+	            case 6:
+	            	testValidate(sc);
+	            	break;
 	        }
 	        n = sc.nextInt();
 	    }
 	}
 	
+	public static void testValidate(Scanner sc) {
+		
+	}
+	
+		public static void listRestrictions() {
+			System.out.println(
+					"---------------------\n"
+		            + "Opciones\n"
+		            + " 1| CorequisitRestriction\n"
+		            + " 2| DayPeriodRestriction\n"
+		            + " 3| LectureFromSameGroupOverlap\n"
+		            + " 4| OccupiedRoomRestriction\n"
+		            + " 5| ParentGroupOverlapRestriction\n"
+		            + " 6| SpecificDayOrHourRestriction"
+		            + " 7| SubjectLevelRestriction"
+		            + " 0| Salir\n"
+		            + "---------------------\n"
+		            );
+		}
+	
 		public static void testConstructor(Scanner sc){
 		    try {
-		    	System.out.println("Definir nueva Restriccion");
-		        System.out.println(">Introduzca si es negociable [S/N]");
-		        
-		        Boolean negotiable = false;
-		        String b = sc.next().trim().toLowerCase();
-		        while(!b.equals("s") && !b.equals("n")) {
-		        	System.out.println("Error: Introduzca 'S' o 'N'");
-		        	b = sc.next().trim().toLowerCase();
-		        }
-		        if(b.equals("s")) negotiable = true;
-		        
-		        r = (Restriction) new SubclassRestrictionStub(negotiable);
-		        System.out.println("Nueva Restriccion definida");
+		    	System.out.println("Escoger restriccion a crear. Solo se guarda la ultima restriccion creada.\n");
+		    	listRestrictions();
+		    	int n;
+			    n = sc.nextInt();
+			    while (n != 0) {
+			        switch (n) {
+			            case 1:
+			            	r = new CorequisitRestriction();
+					        System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");
+			                break;
+			            case 2:
+			            	System.out.println("Introduzca la hora que quiere usar para el mediodia:\n");
+			            	Integer midDay = sc.nextInt();
+			            	Group.DayPeriod dp = null;
+			            	System.out.println("Escoja el periodo de clases del grupo:\n"
+			            			+"---------------------\n"
+			    		            + "Opciones\n"
+			    		            + " 1| MORNING\n"
+			    		            + " 2| AFTERNOON\n"
+			    		            + " 3| INDIFERENT\n"
+			    		            + "---------------------\n");
+			            	int o;
+			            	o = sc.nextInt();
+			            	switch (o) {
+			            		case 1:
+			            			dp = Group.DayPeriod.MORNING;
+			            			break;
+			            		case 2:
+			            			dp = Group.DayPeriod.AFTERNOON;
+			            			break;
+			            		case 3:
+			            			dp = Group.DayPeriod.INDIFERENT;
+			            			break;
+			            	}
+			            	r = new DayPeriodRestriction(midDay, dp);
+			                break;
+			            case 3:
+			            	r = new LectureFromSameGroupOverlapRestriction();
+			            	System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");
+			                break;
+			            case 4:
+			            	r = new OccupiedRoomRestriction();
+			            	System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");
+			                break;
+			            case 5:
+			            	r = new ParentGroupOverlapRestriction();
+			            	System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");
+			                break;
+			            case 6:
+			            	System.out.println("Introduzca dia y/o hora en este orden.\n"
+            						+"en caso de solo dia o hora, introduzca -1 en el campo que no requiera.\n");
+			            	Integer day, hour;
+			            	day = sc.nextInt();
+			            	if (day.equals(-1)) day = null;
+			            	hour = sc.nextInt();
+			            	if (hour.equals(-1)) hour = null;
+			            	r = new SpecificDayOrHourRestriction(day, hour);
+					        System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");	
+					        break;
+			            case 7:
+			            	r = new SubjectLevelRestriction();
+			            	System.out.println("Nueva Restriccion ( " + r.toString() + " ) definida\n");
+			            	break;
+			        }
+			    }
 		    } catch (Exception e) {
 		        System.out.println(e);
 		    }
@@ -73,8 +157,8 @@ public class RestrictionDriver {
 		
 		public static void testDisable(Scanner sc){
 			try {
-	            r.disable();
-	            System.out.println("Disabled");
+	            if (r.disable()) System.out.println("La restriccion ( " + r.toString() + " ) ha sido desactivada.\n");
+	            else System.out.println("La restriccion ( " + r.toString() + " ) no se puede desactivar porque no es negociable.\n");
 	        } catch (Exception e) {
 	            System.out.println(e);
 	        }
@@ -82,8 +166,8 @@ public class RestrictionDriver {
 		
 		public static void testEnable(Scanner sc){
 			try {
-	            r.enable();
-	            System.out.println("Enabled");
+	            if (r.enable()) System.out.println("La restriccion ( " + r.toString() + " ) ha sido activada.\n");
+	            else System.out.println("La restriccion ( " + r.toString() + " ) esta siempre activada porque no es negociable.\n");
 	        } catch (Exception e) {
 	            System.out.println(e);
 	        }
@@ -93,7 +177,8 @@ public class RestrictionDriver {
 		public static void testIsEnabled(Scanner sc){
 			try {
 	            Boolean x = r.isEnabled();
-	            System.out.println("is Enabled: " + x);
+	            if (x) System.out.println("La restriccion ( " + r.toString() + " ) esta ACTIVADA.\n");
+	            else System.out.println("La restriccion ( " + r.toString() + " ) esta DESACTIVADA.\n");
 	        } catch (Exception e) {
 	            System.out.println(e);
 	        }
@@ -102,7 +187,8 @@ public class RestrictionDriver {
 		public static void testIsNegotiable(Scanner sc){
 			try {
 	            Boolean x = r.isNegotiable();
-	            System.out.println("is Negotiable: " + x);
+	            if (x) System.out.println("La restriccion ( \" + r.toString() + \" ) es negociable.\n");
+	            else  System.out.println("La restriccion ( \" + r.toString() + \" ) NO es negociable.\n");
 	        } catch (Exception e) {
 	            System.out.println(e);
 	        }
@@ -110,4 +196,3 @@ public class RestrictionDriver {
 		
 	
 	}
-	*/
