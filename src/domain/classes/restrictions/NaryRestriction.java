ef46950c677ecd2c3b8f8bd795247a8641f54718
xavier.martin.ballesteros <xavier.martin.ballesteros@est.fib.upc.edu>
2018-11-16 02:43:59 +0100

    JAVADOC de Nary i Unary Restrictions

--- src/domain/classes/restrictions/NaryRestriction.java
+++ src/domain/classes/restrictions/NaryRestriction.java
@@ -8,12 +8,30 @@ import domain.classes.PosAssig;
 import domain.classes.Restriction;
 import domain.classes.Subject;
 
+/** Classe que engloba totes les restriccions n-àries.
+ * @author XX
+*/
+
 public abstract class NaryRestriction extends Restriction {
 	
+	/** Constructora estàndard.
+	 */
 	public NaryRestriction(boolean negotiable) {
 		super(negotiable);
 	}
 
+	/** Validació de la restricció.
+	 * @param lecture	Sessió que hem afegit a l'horari.
+	 * @param room		Aula on hem afegit la sessió.
+	 * @param day		Dia en el que hem afegit la sessió.
+	 * @param hour		Hora en la que hem afegit la sessió.
+	 * @param subjects	Conjunt d'assignatures de l'entorn.
+	 * @param groups	Conjunt de grups de l'entorn.
+	 * @param lectures	Conjunt de sessions de l'entorn.
+	 * @param shrek		Conjunt de possibles assignacions per a cada sessió.
+	 * @return True si, un cop eliminat les aules en les que cada sessió no podia anar, totes les sessions restant poden anar com a mínim a una aula. False en cas contrari.
+	 * Les aules que s'eliminaran dependran de la restricció que estiguem validant.
+	*/
 	public abstract boolean validate(String lecture, String room, Integer day, Integer hour, Map<String, Subject> subjects,
 			Map<String, Group> groups, Map<String, Lecture> lectures, Map<String, PosAssig> shrek);
 
--- src/domain/classes/restrictions/UnaryRestriction.java
+++ src/domain/classes/restrictions/UnaryRestriction.java
@@ -2,13 +2,24 @@ package domain.classes.restrictions;
 
 import domain.classes.Restriction;
 
+/** Classe que engloba totes les restriccions un-àries.
+ * @author XX
+*/
 
 public abstract class UnaryRestriction extends Restriction {
-	
-	public abstract boolean validate(Integer day, Integer hour, Integer duration);
 
+	/** Constructora estàndard.
+	 */
 	public UnaryRestriction(boolean negotiable) {
 		super(negotiable);
 	}
 
+	/**
+	 * Validació de la restricció.
+	 * @param day		Dia en que, combinat amb l'hora, no pot anar el grup.
+	 * @param hour		Hora en que, combinada amb el dia, no pot anar el grup.
+	 * @param duration	Duració del grup.
+	 * @return True si el grup es pot posar en el day i hour. False en cas contrari.
+	 */
+	public abstract boolean validate(Integer day, Integer hour, Integer duration);
 }
