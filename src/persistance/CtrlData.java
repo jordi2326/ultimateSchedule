99f80b616e67bb17d28759b0030441f9b3115b18
CarlosBergillos <carlos98.air.cn@gmail.com>
2018-11-16 03:19:26 +0100

    Lot of javadoc and drivers

--- src/persistance/CtrlData.java
+++ src/persistance/CtrlData.java
@@ -8,13 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// import java.io.FileNotFoundException;
-// import org.json.simple.JSONObject;
+import domain.controllers.CtrlDomain;
+
+/** Controlador principal de dades de l'aplicació.
+ * @author XX
+*/
 
 public class CtrlData {
 	
+	/** Instancia d'aquesta classe.
+	*/
 	private static CtrlData instance;
 	
+	/**
+	 * Retorna la instancia d'aquesta classe.
+	 * @return {@link CtrlDomain#instance}
+	 */
 	public static CtrlData getInstance() {
 		if (instance == null)
 			instance = new CtrlData() {
@@ -22,26 +31,49 @@ public class CtrlData {
 		return instance;
 	}
 	
+	/** Constructora estàndard.
+	*/
 	private CtrlData() {
 	}
 	
+	/**
+	 * Importa un entorn (aules, assignatures, aules) desde un arxiu.
+	 * @param filename Nom de l'arxiu d'entorn a importar.
+	 * @return Contingut de l'arxiu llegit.
+	 */
 	public String readEnvironment(String filename) throws FileNotFoundException {
 		return readData("environments/"+filename);
 	}
 	
-	public boolean writeEnvironment(String filename, String content) throws FileNotFoundException {
+	/**public boolean writeEnvironment(String filename, String content) throws FileNotFoundException {
         return writeData("environments/"+filename, content);
-	}
+	}**/
 	
+	/**
+	 * Importa un horari desde un arxiu.
+	 * @param filename Nom de l'arxiu d'horari a importar.
+	 * @return Contingut de l'arxiu llegit.
+	 */
 	public String readSchedule(String filename) throws FileNotFoundException {
 		return readData("schedules/"+filename);
 	}
 	
+	/**
+	 * Guarda el contingut donat (que representa un horari) a un arxiu dins la carpeta d'horaris.
+	 * @param filename Nom amb el que guardar l'horari.
+	 * @param content Contingut de l'horari a guardar.
+	 * @return true si s'ha guardat correctament.
+	 */
 	public boolean writeSchedule(String filename, String content) throws FileNotFoundException {
 		return writeData("schedules/"+filename, content);
 	}
 	
-	public String readData(String filename) throws FileNotFoundException {
+	/**
+	 * Llegeix els continguts d'un arxiu.
+	 * @param filename Adreça de l'arxiu a llegir.
+	 * @return El contingut de l'arxiu.
+	 */
+	private String readData(String filename) throws FileNotFoundException {
 			FileReader fr = new FileReader("data/"+filename);
 			Scanner scan = new Scanner(fr);
 			scan.useDelimiter("\\Z");
@@ -50,6 +82,11 @@ public class CtrlData {
 			return out;
 	}
 	
+	/**
+	 * Escriu un String a un arxiu local.
+	 * @param filename Adreça de l'arxiu a on escriure (si no existeix es crearà).
+	 * @return true si s'ha escrit correctament.
+	 */
 	private boolean writeData(String filename, String content) throws FileNotFoundException {
 		PrintWriter pw = new PrintWriter("data/"+filename); 
         pw.write(content); 
@@ -58,14 +95,27 @@ public class CtrlData {
         return true;
 	}
 	
+	/**
+	 * Retorna una llista de noms d'arxius d'entorn disponibles.
+	 * @return Una llista de noms d'arxius d'entorn disponibles.
+	 */
 	public List<String> getEnvironmentFilesList(){
 		return getFilesList("data/environments");
 	}
 	
+	/**
+	 * Retorna una llista de noms d'arxius d'horari disponibles.
+	 * @return Una llista de noms d'arxius d'horari disponibles.
+	 */
 	public List<String> getScheduleFilesList(){
 		return getFilesList("data/schedules");
 	}
 	
+	/**
+	 * Retorna una llista de noms d'arxius que es troben dins d'una carpeta.
+	 * @param path Adreça de la carpeta.
+	 * @return Una llista de noms d'arxius que es troben dins de la carpeta donada.
+	 */
 	private List<String> getFilesList(String path){
 		File folder = new File(path);
 		File[] listOfFiles = folder.listFiles();
