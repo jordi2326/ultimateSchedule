1d770d5e0cbce04efea941d447229c162b1fdb49
xavier.martin.ballesteros <xavier.martin.ballesteros@est.fib.upc.edu>
2018-11-16 02:23:43 +0100

    JAVADOC de restriccions (Falta NAry i UNary)

--- src/domain/classes/restrictions/CorequisitRestriction.java
+++ src/domain/classes/restrictions/CorequisitRestriction.java
@@ -9,21 +9,38 @@ import domain.classes.PosAssig;
 import domain.classes.Subject;
 
 /** Restricció sobre els correquisits de les assignatures.
- *  Dos grups d'assignatures del mateix nivell no poden anar al mateix dia i hora si tenen el mateix codi i són del mateix tipus.
- *  	Ex: FM 10 T no pot anar al mateix dia i hora que PRO1 10 T, però sí pot anar amb PRO1 11 L i PRO1 20 T.
+ *  Dos grups amb el mateix parentCode de dues assignatures correquisites entre elles no poden anar a un mateix dia i hora.
+ *  	Ex: PROP 10 T no pot anar al mateix dia i hora que TC 15 P, però sí pot anar amb F 11 L i FM 10 T.
  * @author XX
 */
 
 public class CorequisitRestriction extends NaryRestriction{
 	
+	/** Constructora estàndard.
+	 */
 	public CorequisitRestriction() {
 		super(true); //negotiable
 	}
 	
+	/**
+	 * @return El String que identifica la restricció.
+	 */
 	public String toString() {
 		return CorequisitRestriction.class.toString();
 	}
 	
+	/** Validació de la restricció.
+	 * @param lecture	Sessió que hem afegit a l'horari.
+	 * @param room		Aula on hem afegit la sessió.
+	 * @param day		Dia en el que hem afegit la sessió.
+	 * @param hour		Hora en la que hem afegit la sessió.
+	 * @param subjects	Conjunt d'assignatures de l'entorn.
+	 * @param groups	Conjunt de grups de l'entorn.
+	 * @param lectures	Conjunt de sessions de l'entorn.
+	 * @param shrek		Conjunt de possibles assignacions per a cada sessió.
+	 * @return True si, un cop eliminat les aules en les que cada sessió no podia anar, totes les sessions restant poden anar com a mínim a una aula. False en cas contrari.
+	 * Les aules que s'eliminen per cada sessió restant són aquelles que farien anar dos grups (un de cada assignatura correquisites entre elles), amb mateix parentCode, en un dia i hora concrets.
+	*/
 	//Si la assignatura A ï¿½s correq de B, llavors hi ha d'haver alguna combinaciï¿½ en que algun grup de A i un grup que pot ser diferent de B no es solapen
 	public boolean validate(String lecture, String room, Integer day, Integer hour, Map<String, Subject> subjects,
 			Map<String, Group> groups, Map<String, Lecture> lectures, Map<String, PosAssig> shrek) {
